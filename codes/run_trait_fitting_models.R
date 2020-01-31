d2f024b5c11665c57be422a4b269c823237ab1ac
jms5151 <jamie.sziklay@gmail.com>
2020-01-31 16:19:43 -0400

    Created simulated data to test model.

--- codes/run_trait_fitting_models.R
+++ codes/run_trait_fitting_models.R
@@ -6,12 +6,18 @@ library(boot)
 # load data
 gr <- read.csv("C:/Users/Jeremy/Desktop/Vcor_papers/data/Kushmaro_etal_1998_vibrio_ak1_growth_rate.csv", head=T, stringsAsFactors=F)
 
-# set data
-N <- nrow(gr)
-temp <- gr$Temperature
-GR <- gr$Growth_rate_vibrio_ak1
+# simulate data
+temp <- seq(11, 36, 1)
+LHT <- rnorm(length(temp), (0.007 * temp * (temp - 10) * sqrt(37 - temp)), 1) 
+# plot(temp, LHT)
+N <- length(temp)
 # tempNew <- seq(5, 45, 0.1) # temperature gradient to calculate derived quantities over
 # N_new <-length(tempNew)
+
+# use real data
+# N <- nrow(gr)
+# temp <- gr$Temperature
+# LHT <- gr$Growth_rate_vibrio_ak1
 
 # combine data into dataframe
 # df <- data.frame(N=N, temp=temp, GR=GR, tempNew=tempNew, N_new=N_new) 
@@ -20,8 +26,16 @@ GR <- gr$Growth_rate_vibrio_ak1
 traitMod <- stan_model('codes/Briere_trait_fitting.stan')
 
 # pass data to stan and run model
-fit2 <- sampling(binomMod, list(N=N, temp=temp, GR=GR, tempNew=tempNew, N_new=N_new), iter=1000, chains=4)
+fit2 <- sampling(traitMod, list(N=N, temp=temp, LHT=LHT), iter=1000, chains=4) #, tempNew=tempNew, N_new=N_new
 # we want to save fit2 if possible
 
 # assess model fit
 launch_shinystan(fit2)
+
+
+# // generated quantities {
+#   //     for(i in 1:N_new){
+#     //     real mu_new[i] = -1 * constant * (tempNew[i] - T0) * (tempNew[i] - Tm) * (Tm > tempNew[i]) * (T0 < tempNew[i])
+#     //     LHT_new[i] ~ normal(mu_new[i], sigma);
+#     //     }
+#   // }
