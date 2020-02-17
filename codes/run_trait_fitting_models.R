2a996e7268e1b61d017c430fac41bfa1a6486967
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-02-17 17:21:49 +1000

    Update model fitting procedures.

--- codes/run_trait_fitting_models.R
+++ codes/run_trait_fitting_models.R
@@ -3,6 +3,7 @@ library(rstan)
 library(shinystan)
 library(boot)
 
+<<<<<<< HEAD
 # Briere ---------------------------------------------------------
 # simulate data
 temp <- seq(11, 36, 1)
@@ -15,10 +16,25 @@ N_new <-length(tempNew)
 # use real data
 # load data
 # gr <- read.csv("C:/Users/Jeremy/Desktop/Vcor_papers/data/Kushmaro_etal_1998_vibrio_ak1_growth_rate.csv", head=T, stringsAsFactors=F)
+=======
+# load data
+gr <- read.csv("C:/Users/Jeremy/Desktop/Vcor_papers/data/Kushmaro_etal_1998_vibrio_ak1_growth_rate.csv", head=T, stringsAsFactors=F)
+
+# simulate data
+temp <- seq(11, 36, 1)
+LHT <- rnorm(length(temp), (0.007 * temp * (temp - 10) * sqrt(37 - temp)), 1) 
+# plot(temp, LHT)
+N <- length(temp)
+# tempNew <- seq(5, 45, 0.1) # temperature gradient to calculate derived quantities over
+# N_new <-length(tempNew)
+
+# use real data
+>>>>>>> 38d17bcbaba615bdb4ee55b28a8a3c6e056a4132
 # N <- nrow(gr)
 # temp <- gr$Temperature
 # LHT <- gr$Growth_rate_vibrio_ak1
 
+<<<<<<< HEAD
 # compile model
 briere_traitModel <- stan_model('codes/Briere_trait_fitting.stan')
 
@@ -84,3 +100,25 @@ survival <- rbind(s1, s2, s3) %>%
 survival$Infection_probability <- 100-survival$Percent_survival 
 
 infectious_period <- data.frame("Temperature"=c(23,27,29), "Days"=c(3:1))
+=======
+# combine data into dataframe
+# df <- data.frame(N=N, temp=temp, GR=GR, tempNew=tempNew, N_new=N_new) 
+
+# compile model
+traitMod <- stan_model('codes/Briere_trait_fitting.stan')
+
+# pass data to stan and run model
+fit2 <- sampling(traitMod, list(N=N, temp=temp, LHT=LHT), iter=1000, chains=4) #, tempNew=tempNew, N_new=N_new
+# we want to save fit2 if possible
+
+# assess model fit
+launch_shinystan(fit2)
+
+
+# // generated quantities {
+#   //     for(i in 1:N_new){
+#     //     real mu_new[i] = -1 * constant * (tempNew[i] - T0) * (tempNew[i] - Tm) * (Tm > tempNew[i]) * (T0 < tempNew[i])
+#     //     LHT_new[i] ~ normal(mu_new[i], sigma);
+#     //     }
+#   // }
+>>>>>>> 38d17bcbaba615bdb4ee55b28a8a3c6e056a4132
