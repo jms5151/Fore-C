1b74d7abe56505c3d2abd72c85df37c538b475e5
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-03-26 17:20:07 +1000

    Increased number of simulated predictor variables, removed real data.

--- codes/run_binomial_regression_stan.R
+++ codes/run_binomial_regression_stan.R
@@ -1,3 +1,5 @@
+rm(list=ls()) #remove previous variable assignments
+
 # load library
 library(rstan)
 library(shinystan)
@@ -5,34 +7,24 @@ library(boot)
 
 options(mc.cores=4)
 
-# load real data ----------
-load("merged_data_tls.R")
-N <- nrow(tls)
-C <- tls$C
-Y <- tls$Y
-X <- tls$HotSnap
-M <- 1 # dimensions of x (# driver vars)
-
-# p <- tls$p
-# par(mfrow=c(2,2))
-# plot(X,p, pch=16, xlab="Temperature", ylab="Prevalence")
-# title("actual data")
-# plot(X,mu, pch=16, xlab="Temperature")
-# plot(p,Y, pch=16, xlab="Prevalence", ylab="# diseased colonies")
-# plot(X,Y, pch=16, xlab="Temperature", ylab="# diseased colonies")
-
-# simulate data -----------
+# simulate data with multiple predictor variables -----------
 N <- 100 # number of sites
 C <- round(runif(N, min=10, max=100)) # number of colonies on a transect
-X <- t(scale(rnorm(N,27,2))) # driver variable 1: temperature values at each transect
+temp <- scale(rnorm(N,27,1)) # driver variable 1: temperature values at each transect
+chl <- scale(rnorm(N,0.14,0.08)) # driver variable 2: chl-a values at each transect
+dev <- scale(rnorm(N,18,10))
+X <- t(data.frame(temp, chl, dev))
 b0 <- -3 # intercept
 b1 <- 1 # slope (increase in log odds of disease per unit x)
-mu <- (X * b1 + b0) # generating function to describe logit prevalence
+b2 <- 1
+b3 <- 1
+mu <- (X[1,]*b1 + X[2,]*b2 + X[3,]*b3 + b0) # generating function to describe logit prevalence
 p <- inv.logit(mu) # prevalence 
-M <- 1 # dimensions of x (# driver vars)
+M <- 3 # dimensions of x (# driver vars)
 Y <- rbinom(N,C,p) # number of diseased colonies on each transect
+
 # plot simulated data -----
-# plot(X,p, pch=16, xlab="Temperature", ylab="Prevalence")
+# plot(X[1,],p, pch=16, xlab="Temperature", ylab="Prevalence")
 # title("simulated data")
 # plot(X,mu, pch=16, xlab="Temperature")
 # plot(p,Y, pch=16, xlab="Prevalence", ylab="# diseased colonies")
@@ -40,17 +32,27 @@ Y <- rbinom(N,C,p) # number of diseased colonies on each transect
 
 # new data to generate -----
 N_new <- 1000
-X_new <- t(seq(from=min(X), to=max(X), length=N_new))
-
-# combine data into dataframe
-df <- data.frame(N=N, C=C, X=X, M=M, Y=Y, N_new=N_new, X_new=X_new) 
+temp_new <- seq(from=min(X[1,]), to=max(X[1,]), length=N_new)
+chl_new <- seq(from=min(X[2,]), to=max(X[2,]), length=N_new)
+dev_new <- seq(from=min(X[3,]), to=max(X[3,]), length=N_new)
+X_new <- t(data.frame(temp_new, chl_new, dev_new))
 
 # compile model
 binomMod <- stan_model('codes/binomial_model.stan')
 
 # pass data to stan and run model
 fit2 <- sampling(binomMod, list(N=N, C=C, X=X, M=M, Y=Y, N_new=N_new, X_new=X_new), iter=1000, chains=4)
-# we want to save fit2 if possible
 
 # assess model fit
 launch_shinystan(fit2)
+
+# Output ---------------------------------------------------------------------
+# extract data
+list_of_draws <- extract(fit2)
+ga <- data.frame(cbind(colMeans(list_of_draws$Y_ppc), list_of_draws$beta[1], list_of_draws$beta[2], list_of_draws$beta[3], list_of_draws$b0, colMeans(list_of_draws$theta)))
+colnames(ga) <- c("Y_ppc", "b1", "b2", "b3", "b0", "theta")
+
+# plot
+plot(X[1,], inv.logit(median(ga$b0) + median(ga$b1)*X[1,] + median(ga$b2)*X[2,] + median(ga$b3)*X[3,]), pch=16, xlab='Temperature', ylab='mu')
+plot(X[2,], inv.logit(median(ga$b0) + median(ga$b1)*X[1,] + median(ga$b2)*X[2,] + median(ga$b3)*X[3,]), pch=16, xlab='Chl-a', ylab='mu')
+plot(X[3,], inv.logit(median(ga$b0) + median(ga$b1)*X[1,] + median(ga$b2)*X[2,] + median(ga$b3)*X[3,]), pch=16, xlab='Development', ylab='mu')
