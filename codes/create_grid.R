7dd5bae78102cc11c48def3dca390139b7a96523
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-04-15 18:37:51 +1000

    Update grid to correspond with NOAA Coral Reef Watch experimental product mask for reefs and subset to areas of interest.

--- codes/create_grid.R
+++ codes/create_grid.R
@@ -1,29 +1,37 @@
 # create grid for fore-c map based on CRW virtual station pixels ---------------------
 rm(list=ls()) #remove previous variable assignments
 
-# Great Barrier Reef, Australia
-gbr <- expand.grid("Longitude"=seq(140.5,155.5,1), "Latitude"=seq(-26.5,-9.5,1))
+# libraries
+library(raster)
 
-# Hawaii
-hawaii <- expand.grid("Longitude"=seq(-178.5,-153.5,1), "Latitude"=seq(16.5,37.5,1))
+# Open the NC file as a raster brick 
+thermHistBrick <- brick("Data/noaa_crw_thermal_history_sst_trend_v2.1.nc", varname="mask")
 
-# Howland and Baker Atolls
-howland_baker <- expand.grid("Longitude"=seq(-178.5,-174.5,1), "Latitude"=seq(-1.5,2.5,1))
+# Turn the brick into a points file
+reefMaskPts <- as.data.frame(rasterToPoints(x = thermHistBrick[[1]]))
 
-# Johnston Atoll
-johnston <- expand.grid("Longitude"=seq(-170.5,-168.5,1), "Latitude"=seq(15.5,17.5,1))
+# Change the names of the columns
+colnames(reefMaskPts) <- c("Longitude", "Latitude", "ReefMask")
 
-# Marianas
-marianas <- expand.grid("Longitude"=seq(140.5,150.5,1), "Latitude"=seq(11.5,21.5,1))
+# Only keep the points equal to 1
+reefMaskPts <- subset(reefMaskPts, ReefMask == 1)
 
-# Samoas
-samoas <- expand.grid("Longitude"=seq(-174.5,-166.5,1), "Latitude"=seq(-16.5,-8.5,1))
+# remove ReefMask column
+grid <- reefMaskPts[,c("Longitude", "Latitude")]
 
-# Wake Atoll
-wake <- expand.grid("Longitude"=seq(165.5,168.5,1), "Latitude"=seq(17.5,21.5,1))
+# add region name for regions of interest 
+grid$Region <- NA
+grid$Region[grid$Longitude > 140.5 & grid$Longitude < 146.5 & grid$Latitude > -12.1 & grid$Latitude < -9] <- "GBR"
+grid$Region[grid$Longitude > 140.5 & grid$Longitude < 155.5 & grid$Latitude > -26.5 & grid$Latitude < -12.1] <- "GBR"
+grid$Region[grid$Longitude > -178.5 & grid$Longitude < -153.5 & grid$Latitude > 16.5 & grid$Latitude < 37.5] <- "Hawaii"
+grid$Region[grid$Longitude > -178.5 & grid$Longitude < -174.5 & grid$Latitude > -1.5 & grid$Latitude < 2.5] <- "PRIAs"
+grid$Region[grid$Longitude > -170.5 & grid$Longitude < -168.5 & grid$Latitude > 15.5 & grid$Latitude < 17.5] <- "PRIAs"
+grid$Region[grid$Longitude > 165.5 & grid$Longitude < 168.5 & grid$Latitude > 17.5 & grid$Latitude < 21.5] <- "PRIAs"
+grid$Region[grid$Longitude > 140.5 & grid$Longitude < 150.5 & grid$Latitude > 11.5 & grid$Latitude < 21.5] <- "Marianas"
+grid$Region[grid$Longitude > -174.5 & grid$Longitude < -166.5 & grid$Latitude > -16.5 & grid$Latitude < -8.5] <- "Samoa"
 
-# combine grids
-grid <- do.call("rbind", list(gbr, hawaii, howland_baker, johnston, marianas, samoas, wake))
+# subset grid to regions of interest
+grid <- subset(grid, !is.na(Region))
 
 # save grid
 save(grid, file="Compiled_data/grid.RData")
