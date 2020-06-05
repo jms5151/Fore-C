4ff047dd96534346672b5d1e64afdc30e1b9eebb
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-06-05 23:03:28 +1000

    Initial commit.

---
+++ codes/addScaleBar.R
@@ -0,0 +1,13 @@
+addScaleBar = function(map,
+                       position = c('topright', 'bottomright', 'bottomleft', 'topleft'),
+                       options = scaleBarOptions()) {
+  
+  options = c(options, list(position = match.arg(position)))
+  invokeMethod(map, getMapData(map), 'addScaleBar', options)
+}
+
+scaleBarOptions = function(maxWidth = 100, metric = TRUE, imperial = TRUE,
+                           updateWhenIdle = TRUE) {
+  list(maxWidth=maxWidth, metric=metric, imperial=imperial,
+       updateWhenIdle=updateWhenIdle)
+}
