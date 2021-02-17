a048424ad8e7113fb15aedaa8e01254b8a3a3df5
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2021-02-17 10:56:14 +1000

    inital commit

---
+++ codes/initial_survey_formatting.R
@@ -0,0 +1,71 @@
+## Initial formatting of survey data
+
+# load libraries ----------------------
+library(tidyverse)
+library(data.table) 
+library(taxize)
+
+# load data ---------------------------
+# NOAA Ecosystem Science Division data
+load("Data/ESD2008_2012dataFOREC.Rdata")
+esd0812 <- x 
+
+load("Data/ESD2013_2017dataFOREC.Rdata")
+esd1317 <- x
+
+# Hawaii Coral Disease Database (HICORDIS) data
+hicordis <- read.csv( "Data/hicordis.csv", head = T, stringsAsFactors = F)
+
+# University of Guam data
+guamRaw <- read.csv("Data/HEALTH_IMPACTS_DATABASE_FOR_NOAA.csv", stringsAsFactors = F, head = T)
+guamCodes <- read.csv("Data/species_codes.csv", stringsAsFactors = F, head = T)
+guam.gps <- read.csv("Data/guam_sites_with_gps.csv", stringsAsFactors = F, head = T)
+
+# Great Barrier Reef (GBR) Reef Health and Impact Surveys (RHIS) data
+gbr <- read.csv("Data/GBRMPA_RHIS_01012009-18092020.csv", stringsAsFactors = F, head = T)
+
+# format ----------------------------
+# dates
+esd0812$Date <- as.Date(esd0812$DATE_, "%d-%b-%y")
+esd1317$Date <- as.Date(esd1317$DATE_, "%d-%b-%y")
+hicordis$Date <- as.Date(hicordis$Date, "%Y-%m-%d")
+guamRaw$Date <- as.Date(guamRaw$DATE, "%m/%d/%Y")
+gbr$Date <- as.Date(gbr$Observation.Date, "%m/%d/%Y")
+
+# edit projects from HICRODIS data
+hicordis <- subset(hicordis, Project != "CRED")
+hicordis$Project[hicordis$Project == "Aeby" | hicordis$Project == "Kenyon"] <- "Aeby_Kenyon"
+
+# add info to RHIS data
+gbr$Region <- "GBR"
+gbr$Island <- gbr$Marine.Park.Mgmt.Section # for case-control grouping
+gbr$Project <- "RHIS"
+
+# add info to Guam data
+guamRaw <- guamRaw %>% left_join(guam.gps, by="SITE")
+
+# format taxonomy
+acroporids <- children("Acroporidae", db = "worms") # list species by coral family indicated
+poritids <- children("Poritidae", db = "worms")
+
+esd0812$Family <- ifelse((esd0812$GENUS %in% acroporids$Acroporidae$childtaxa_name) == TRUE, "Acroporidae", NA)
+esd0812$Family <- ifelse((esd0812$GENUS %in% poritids$Poritidae$childtaxa_name) == TRUE, "Poritidae", esd0812$Family)
+
+esd1317$Family <- ifelse((esd1317$GENUS %in% acroporids$Acroporidae$childtaxa_name) == TRUE, "Acroporidae", NA)
+esd1317$Family <- ifelse((esd1317$GENUS %in% poritids$Poritidae$childtaxa_name) == TRUE, "Poritidae", esd1317$Family)
+
+hicordis$Family <- ifelse((hicordis$Genus %in% acroporids$Acroporidae$childtaxa_name) == TRUE, "Acroporidae", NA)
+hicordis$Family <- ifelse((hicordis$Genus %in% poritids$Poritidae$childtaxa_name) == TRUE, "Poritidae", hicordis$Family)
+
+guamRaw <- guamRaw %>% left_join(guamCodes, by="SPECIES")
+guamRaw$Genus <- gsub("([A-Za-z]+).*", "\\1", guamRaw$Species)
+guamRaw$Family <- ifelse((guamRaw$Genus %in% acroporids$Acroporidae$childtaxa_name) == TRUE, "Acroporidae", NA)
+guamRaw$Family <- ifelse((guamRaw$Genus %in% poritids$Poritidae$childtaxa_name) == TRUE, "Poritidae", guamRaw$Family)
+
+# fix errors -------------------------
+# fix Guam coordinate error
+xold <- guam.gps$longitude[guam.gps$SITE == "LUM"]
+yold <- guam.gps$latitude[guam.gps$SITE == "LUM"]
+
+guam.gps$longitude[guam.gps$SITE == "LUM"] <- yold
+guam.gps$latitude[guam.gps$SITE == "LUM"] <- xold
