66ce3ecf21bfa4ea80ce60703d1d1b739519015b
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2021-03-05 14:41:04 +1000

    reset extraction method to limit NA values and resummarise

--- codes/extract_sst_metrics_to_surveys.R
+++ codes/extract_sst_metrics_to_surveys.R
@@ -1,4 +1,4 @@
-# Extract SST metrics to survey data ---------------
+# Extract SST metrics to survey data ---------------------------------------------------------------
 
 # load libraries
 library(tidyverse)
@@ -8,67 +8,63 @@ library(raster)
 # load surey data
 load("Compiled_data/Survey_points_with_dates.RData")
 
-# SST anomalies ------------------------------------
-# surveyDatesRaw <- sort(unique(surveys$Date))
-# surveyDates <- gsub("-", "", surveyDatesRaw)
-# 
-# newFileName <- "Compiled_data/sst_anomalies_surveys.csv"
-# ssta.df <- data.frame(matrix(ncol = 4, nrow = 0))
-# colnames(ssta.df) <- c("Longitude", "Latitude", "Date", "SST_anomalies")
-# write.csv(ssta.df, newFileName, row.names = F)
-# 
-# for(i in 1:length(surveyDates)){
-#   yr <- substr(surveyDates[i], 1, 4)
-#   ncFileName <- paste0("E:/SSTanomaly_CRW/", yr, "/ct5km_ssta_v3.1_", surveyDates[i], ".nc")
-#   ncTempBrick <- brick(ncFileName)
-#   # do not worry about warning messages, they are providing information about the "brick" function
-#   surveySSTa <- extract(ncTempBrick, cbind(surveys$Longitude, surveys$Latitude))
-#   temp.df <- data.frame("Longitude" = surveys$Latitude, "Longitude" = surveys$Longitude, "Date" = surveyDatesRaw[i], "SST_anomalies" = surveySSTa[,])
-#   write.table(temp.df, file = newFileName, row.names = F, sep = ",", col.names = !file.exists(newFileName), append = T)
-#   cat("finished", i, "of", length(surveyDates), ":", as.character(surveyDatesRaw[i]), "\n") # uncomment if you want a print message of progress
-# }
-# 
-# # format and merge sst.df data with original survey data
-# sstaDF <- read_csv(newFileName)
-# # calculate max anomaly in prior 60 days
-# surveys_ssta <- surveys %>% left_join(sstaDF, by = c("Date", "Latitude", "Longitude"))
-# # surveys_ssta <- merge(surveys, sstaDF, by = c("Date", "Latitude", "Longitude"), all.x = T)
-# save(surveys_ssta, file = "Compiled_data/SSTa_surveys.RData")
-
-# SST ---------------------------------------
+# expand dataframe to include row for all 90 days prior to survey date  ----------------------------
 surveys$Survey_Date <- surveys$Date
 
-# expand dataframe to include row for all 90 days prior to survey date 
 surveys_90d <- surveys %>%
   group_by(Survey_Date, Latitude, Longitude) %>%
   complete(Date = seq.Date(Date - 90, Date, by="day"))# %>%
 
 save(surveys_90d, file = "Compiled_data/suveys_90d_expanded.RData")
+
+# extract SST data ---------------------------------------------------------------------------------
 load("Compiled_data/suveys_90d_expanded.RData")
 
 dates_long <- sort(unique(surveys_90d$Date))
 surveyDates2 <- gsub("-", "", dates_long)
 
-# extract SST data
 newFileName2 <- "Compiled_data/sst_surveys.csv"
 sst.df <- data.frame(matrix(ncol = 4, nrow = 0))
-colnames(sst.df) <- c("Longitude", "Latitude", "Date", "SST")
+colnames(sst.df) <- c("Latitude", "Latitude", "Date", "SST")
 write.csv(sst.df, newFileName2, row.names = F)
 
-# check if dates_long exceeds dates of downloaded data, max date == 2020-09-17
 for(i in 1:length(surveyDates2)){
   yr <- substr(surveyDates2[i], 1, 4)
-  ncFileName <- paste0("E:/SST_CoralReefWatch/", yr, "/coraltemp_v1.0_", surveyDates2[i], ".nc")
+  ncFileName <- paste0("I:/SST_CoralReefWatch/", yr, "/coraltemp_v1.0_", surveyDates2[i], ".nc") # on laptop it's E:/
   ncTempBrick <- brick(ncFileName, varname = "analysed_sst")
   # do not worry about warning messages, they are providing information about the "brick" function
   df <- subset(surveys_90d, Date == dates_long[i])
-  surveySST <- extract(ncTempBrick, cbind(df$Longitude, df$Latitude)) # this may need an expansion to not get NAs
-  temp.df <- data.frame("Longitude" = df$Latitude, "Longitude" = df$Longitude, "Date" = dates_long[i], "SST" = surveySST[,])
+  surveySST <- extract(ncTempBrick, cbind(df$Longitude, df$Latitude))
+  if(anyNA(surveySST) == TRUE){
+    surveySST <- extract(ncTempBrick, cbind(df$Longitude, df$Latitude), method = "bilinear")
+    temp.df <- data.frame("Latitude" = df$Latitude, "Longitude" = df$Longitude, "Date" = dates_long[i], "SST" = surveySST)
+  } else {
+    temp.df <- data.frame("Latitude" = df$Latitude, "Longitude" = df$Longitude, "Date" = dates_long[i], "SST" = surveySST[,])
+  }
   write.table(temp.df, file = newFileName2, row.names = F, sep = ",", col.names = !file.exists(newFileName2), append = T)
-  cat("finished", i, "of", length(surveyDates2), ":", as.character(dates_long[i]), "\n") # uncomment if you want a print message of progress
+  # indicate progress periodically
+  if(i %in% seq(500, 5000, by = 500)){
+    cat("finished", i, "of", length(surveyDates2), ":", as.character(dates_long[i]), "\n") # uncomment if you want a print message of progress
+  }
 }
 
-# need to update this, and don't need rollapply, just mean by group of date/lat/lon (there should be 3509597 rows of data)
-surveys_sst <- read_csv("Compiled_data/sst_surveys.csv")
-surveys_sst$SST_mean90d <- rollapply(surveys_sst$SST, width = 90, FUN = mean, align = "right", fill = NA, na.rm = T) 
-save(surveys_sst, file = "Compiled_data/sst_surveys_90dMean.RData")
+
+
+# calculate 90 day mean sst for all surveys -------------------------------------------------------
+# load data from above
+load("Compiled_data/suveys_90d_expanded.RData")
+sst <- read.csv("Compiled_data/sst_surveys.csv")
+
+# format data
+surveys_90d <- as.data.frame(surveys_90d)
+sst$Date <- as.Date(sst$Date,"%Y-%m-%d")
+
+# make SST calculations and rename survey date
+sst_90d <- surveys_90d %>%
+  left_join(sst, by = c("Date", "Latitude", "Longitude")) %>%
+  group_by(Survey_Date, Longitude, Latitude) %>%
+  summarise(SST_90dMean = mean(SST, na.rm = T)) %>%
+  rename(Date = Survey_Date)
+
+# save
+save(sst_90d, file = "Compiled_data/surveys_sst_90dMean.RData")
