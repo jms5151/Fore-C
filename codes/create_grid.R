f1c897facb64f62baaa074b332f79eeb9f137f3f
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2021-03-09 13:44:07 +1000

    save data as csv and rds

--- codes/create_grid.R
+++ codes/create_grid.R
@@ -1,4 +1,4 @@
-# create grid for fore-c map based on CRW virtual station pixels ---------------------
+# create grid for fore-c map ----------------------------------------------------
 rm(list=ls()) #remove previous variable assignments
 
 # Read in reef mask CSV. 0 = water, 1 = reef
@@ -17,6 +17,9 @@ reefLon<- lon5km[inds[,2]]
 reefsDF <- data.frame("Longitude" = reefLon, "Latitude" = reefLat)
 reefsDF$ID <- seq(1, nrow(reefsDF))
 
+# save grid as csv
+save(reefsDF, file = "Compiled_data/grid.RData")
+
 # create raster from point data
 reefsDF <- rasterFromXYZ(reefsDF)
 
@@ -25,6 +28,9 @@ crs(reefsDF) <- CRS("+init=epsg:4326")
 
 # create spatial polygon from raster
 reefs2 <- as(reefsDF, "SpatialPolygonsDataFrame")
+
+# save spatial polygon
+save(reefs2, file = "Compiled_data/spatial_grid.Rds")
 
 # check map 
 leaflet() %>%
