69f5a9230c00486c8cfb9324c118c3ed16ef2827
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2021-03-26 14:24:23 +1000

    fix array order and sequence stop points to match R array

--- codes/create_array.py
+++ codes/create_array.py
@@ -7,11 +7,11 @@ Created on Thu Mar 25 12:01:40 2021
 import numpy as np
 
 # create data
-medColSize = np.arange(0, 200, 50)
+medColSize = np.arange(0, 201, 50) # add one number to send of even numbers used in R
 FishAbundance = np.arange(0.01, 1., 0.3)
 Lights = np.arange(0, 255, 50)
 waveSD = np.arange(0, 42, 20)
-SST = np.arange(20, 30, 2)
+SST = np.arange(20, 31, 2)
 months  = np.arange(1, 12, 2)
 
 # get sizes of data
@@ -27,8 +27,10 @@ Lmonths = months.size
 Nunique = Lcol * Lfish * Llights * LwaveSD * Lsst * Lmonths
 
 # create array
-gangArray = np.arange(Nunique).reshape(Lcol, Lfish, Llights, LwaveSD, Lsst, Lmonths)
+gangArray = np.arange(Nunique).reshape(Lcol, Lfish, Llights, LwaveSD, Lsst, Lmonths, order = 'F')
 
 # check dimensions are correct
 gangArray.shape
 
+# compare id with R (in R: gangArray[2,1,2,1,3,1] = "V742")
+gangArray[1,0,1,0,2,0] # = 741 (equivalent to "V742" in R)
